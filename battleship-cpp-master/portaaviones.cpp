252695e822fbde006a025c0ed418581c6ac6fef4
wasaki <88863852+wasaki2697@users.noreply.github.com>
2022-08-29 23:48:38 -0300

    Delete battleship-cpp-master directory

--- battleship-cpp-master/portaaviones.cpp
+++
@@ -1,5 +0,0 @@
-#include "portaaviones.h"
-
-char nombreNavio[] = "Portaaviones";
-Portaaviones::Portaaviones(int x, int y, char orientacion)
-    : Barco(x, y, 5, orientacion, nombreNavio) {}
--- battleship-cpp-master/tableroataques.cpp
+++
@@ -1,8 +0,0 @@
-#include "tableroataques.h"
-
-TableroAtaques::TableroAtaques(int dimension) : Tablero(dimension) {}
-
-bool TableroAtaques::recibirAtaque(int x, int y) {
-  this->matriz[x][y] = Codigo::Ataque;
-  return true;
-}
